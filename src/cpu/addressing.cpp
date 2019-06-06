dbc85815af2f43640af99ec4da0cdc66185bc47e
ColeCachoo <cole.cassise@outlook.com>
2019-06-06 01:35:44 -0400

    Slightly reduced line count

--- src/cpu/addressing.cpp
+++ src/cpu/addressing.cpp
@@ -3,7 +3,7 @@
 //
 #include "cpu.h"
 
-uint16_t 
+uint8_t 
 CPU::immediate() {
     return fetch();
 }
@@ -31,41 +31,34 @@ CPU::relative() {
 uint16_t 
 CPU::absolute() {
     uint16_t addr_low  = fetch();
-    uint16_t addr_high = fetch();
-    addr_high <<= 8;
+    uint16_t addr_high = fetch() << 8;
     return (addr_high | addr_low);
 }
 
 uint16_t 
 CPU::absolute_x() {
     uint16_t addr_low  = fetch();
-    uint16_t addr_high = fetch();
-    addr_high <<= 8;
+    uint16_t addr_high = fetch() << 8;
     return (addr_high | addr_low) + x_index;
 }
 
 uint16_t 
 CPU::absolute_y() {
     uint16_t addr_low  = fetch();
-    uint16_t addr_high = fetch();
-    addr_high <<= 8;
+    uint16_t addr_high = fetch() << 8;
     return (addr_high | addr_low) + y_index;
 }
 
 uint16_t 
 CPU::indirect() {
     uint16_t addr_low  = fetch();
-    uint16_t addr_high = fetch();
-    addr_high <<= 8;
-    uint16_t new_low  = ram->read(addr_high | addr_low);
+    uint16_t addr_high = fetch() << 8;
+    uint16_t new_low = ram->read(addr_high | addr_low);
     // An original 6502 has does not correctly fetch the target address if the
     // indirect vector falls on a page boundary (e.g. $xxFF where xx is any value
     // from $00 to $FF). In this case fetches the LSB from $xxFF as expected but
     // takes the MSB from $xx00.
-    uint16_t new_high = ram->read(addr_high | uint8_t(addr_low + 1));
-    // This would fix the bug, left in case I need it.
-    // uint16_t new_high = ram->read((addr_high | addr_low) + 1);
-    new_high <<= 8;
+    uint16_t new_high = ram->read(addr_high | uint8_t(addr_low + 1)) << 8;
     return (new_high | new_low);
 }
 
@@ -73,7 +66,6 @@ uint16_t
 CPU::indexed_indirect() {
     uint8_t val = fetch();
     return ram->read((val + x_index) % 256) + ram->read((val + x_index + 1) % 256) * 256;
-
 }
 
 uint16_t 
