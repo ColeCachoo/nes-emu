e40c1373a550a06160fb9901eaa7627d8ff6e3f7
ColeCachoo <cole.cassise@outlook.com>
2019-06-10 21:48:17 -0400

    Renamed file

--- src/error.h
+++
@@ -1,7 +0,0 @@
-// error.h : Includes custom error type and definitions used for error handling.
-//
-#pragma once
-
-using      error_t = bool;
-const bool ERROR   = true;
-const bool SUCCESS = false;
