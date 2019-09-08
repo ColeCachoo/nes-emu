f7eb56ec5561e289c4194e8c36267b887de26f86
ColeCachoo <cole.cassise@outlook.com>
2019-09-08 18:46:46 -0400

    Changed macros to constexpr functions

--- src/nes-utils.h
+++ src/nes-utils.h
@@ -6,25 +6,25 @@
 #include <fmt/format.h>
 
 /// Gets number with only the n'th bit set. Bit position starts from 0.
-#define bit_pos(n) (1 << (n))
+// #define bit_pos(n) (1 << (n))
+template<typename T>
+constexpr auto bit_pos(T n) { return 1 << n; }
 
 /// Sets bits with the position(s) given by flag(s).
 /// Returns result of the operation with the bits set.
-#define set_bit(num, pos) ((num) | (pos))
+// #define set_bit(word, pos) ((word) | (pos))
+template<typename W, typename P>
+constexpr W set_bit(W word, P pos) { return word | pos; }
 /// Clears bits with the position(s) given by flag(s).
 /// Returns result of the operation with the bits cleared.
-#define clear_bit(num, pos) ((num) & (~(pos)))
+// #define clear_bit(word, pos) ((word) & (~(pos)))
+template<typename W, typename P>
+constexpr W clear_bit(W word, P pos) { return word & (~pos); }
 
-/// Gets higher 8 bits from given number.
+/// Gets higher 8 bits from given word.
 template<typename T>
-constexpr uint8_t high_byte(T num)
-{
-    return num >> (sizeof(T) * 8 - 8);
-}
+constexpr uint8_t high_byte(T num) { return num >> (sizeof(T) * 8 - 8); }
 
-/// Gets lower 8 bits from given number.
+/// Gets lower 8 bits from given word.
 template<typename T>
-constexpr uint8_t low_byte(T num)
-{
-    return (uint8_t) num;
-}
+constexpr uint8_t low_byte(T num) { return (uint8_t) num; }
