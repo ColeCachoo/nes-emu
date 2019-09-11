51a2e34e4980c5bf67d60b2de658cb8fb058b79f
ColeCachoo <cole.cassise@outlook.com>
2019-09-11 11:12:55 -0400

    set and clear bit are now macros

--- src/nes-utils.h
+++ src/nes-utils.h
@@ -6,25 +6,23 @@
 #include <fmt/format.h>
 
 /// Gets number with only the n'th bit set. Bit position starts from 0.
-// #define bit_pos(n) (1 << (n))
-template<typename T>
-constexpr auto bit_pos(T n) { return 1 << n; }
+#define BIT_POS(n) (1 << (n))
 
 /// Sets bits with the position(s) given by flag(s).
 /// Returns result of the operation with the bits set.
-// #define set_bit(word, pos) ((word) | (pos))
-template<typename W, typename P>
-constexpr W set_bit(W word, P pos) { return word | pos; }
+#define SET_BIT(word, pos) ((word) | (pos))
+// constexpr uint8_t set_bit(uint8_t word, uint8_t pos) { return word | pos; }
+
 /// Clears bits with the position(s) given by flag(s).
 /// Returns result of the operation with the bits cleared.
-// #define clear_bit(word, pos) ((word) & (~(pos)))
-template<typename W, typename P>
-constexpr W clear_bit(W word, P pos) { return word & (~pos); }
+#define CLEAR_BIT(word, pos) ((word) & (~(pos)))
+// constexpr uint8_t clear_bit(uint8_t word, uint8_t pos) { return word & (~pos); }
 
 /// Gets higher 8 bits from given word.
-template<typename T>
-constexpr uint8_t high_byte(T word) { return word >> (sizeof(T) * 8 - 8); }
+// template<typename T>
+// constexpr uint8_t high_byte(uint16_t word) { return word >> (sizeof(T) * 8 - 8); }
+constexpr uint8_t high_byte(uint16_t word) { return uint8_t(word >> 8); }
 
 /// Gets lower 8 bits from given word.
-template<typename T>
-constexpr uint8_t low_byte(T word) { return uint8_t(word); }
+// template<typename T>
+constexpr uint8_t low_byte(uint16_t word) { return uint8_t(word); }
